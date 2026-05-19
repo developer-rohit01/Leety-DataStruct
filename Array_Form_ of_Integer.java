b92573db122930e3faf847d3419102df79e925d2
Rohit <rr5450534@gmail.com>
2026-05-19 20:33:40 +0530

    Add the Array-Form of Integer LeetCode_989

---
+++ Array_Form_ of_Integer.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections; 
+import java.util.List;
+
+
+
+class Array_Form_of_Integer {
+
+  public static void main(String[] args) {
+    Array_Form_of_Integer s = new Array_Form_of_Integer();
+    int[] num = {1,2,0,0};
+    int k = 34;
+    s.addToArrayForm(num, k);
+  }
+
+    public List<Integer> addToArrayForm(int[] num, int k) {
+       List<Integer> ans = new ArrayList<>();
+
+       int p = num.length-1 ;
+       int carry = 0 ;
+
+       while(p>=0 || k>0){
+           int numval = 0;
+           if(p>=0){
+               numval = num[p];
+           }
+           int d = k%10; // it is the last digit from k
+           int sum = numval + d + carry;
+           int digit = sum%10;
+           carry = sum/10;
+           ans.add(digit);
+           p--;       // moving the pointer
+           k = k/10 ; // removing the last digit from k
+       }
+
+       if(carry>0){
+           ans.add(carry); // adding carry to the array list
+       }
+
+       Collections.reverse(ans);  // reversing the ans array list
+        return ans;
+     }
+}
