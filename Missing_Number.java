c57d074e5e8b342b269be04c67df6a60665a494f
Rohit <rr5450534@gmail.com>
2026-05-22 21:28:16 +0530

    Add the Missing_Number find in Array code LeetCode 268

---
+++ Missing_Number.java
@@ -0,0 +1,21 @@
+class Missing_Number {
+     ;
+  public static void main(String[] args) {
+     Missing_Number s = new Missing_Number();
+    int[] num = {3,0,1};
+    s.MissingNumber(num);
+  }
+
+    public int MissingNumber(int[] nums) {
+        int range = nums.length;
+        int actualSum =  (range * (range + 1))/2;
+        int currSum = 0 ;
+        for(int i=0;i<nums.length;i++){
+            currSum = currSum + nums[i];
+        }
+        int ans = actualSum - currSum ;
+         System.out.println(ans);
+        return ans;
+       
+    }
+}
